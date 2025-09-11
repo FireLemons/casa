7144ad721088c217e363d80983aaca60d559f74f
Shen Yang <shenyang128@proton.me>
2025-09-10 21:23:28 -0500

    stubs for seed_addresses

--- spec/seeds/record_creation_api_spec.rb
+++ spec/seeds/record_creation_api_spec.rb
@@ -162,4 +162,66 @@ RSpec.describe RecordCreator do
       }.to raise_error(ArgumentError, /cannot use user: and user_id:/)
     end
   end
+
+  describe "seed_addresses" do
+    describe "with valid parameters" do
+      it "returns an array containing the addresses created" do
+        #create(:case_contact)
+        #original_additional_expense_count = AdditionalExpense.count
+        #additional_expense_seed_count = 2
+
+        #expect {
+        #  subject.seed_additional_expenses(case_contacts: CaseContact.all, count: additional_expense_seed_count)
+        #}.to change { AdditionalExpense.count }.from(original_additional_expense_count).to(original_additional_expense_count + additional_expense_seed_count)
+      end
+
+      it "returns an array containing an error for each address that could not be created" do
+        # error_array = subject.seed_additional_expenses(case_contact_ids: [-1], count: 2)
+
+        # error_array.each do |error|
+        #   expect(error.message).to include("AdditionalExpense failed to save")
+        # end
+      end
+
+      it "returns empty array for negative counts" do
+        # expect(subject.seed_additional_expenses(case_contact_ids: [1], count: -1)).to eq([])
+      end
+    end
+
+    it "throws an error when neither users or user_ids are used" do
+      # expect {
+      #   subject.seed_additional_expenses
+      # }.to raise_error(ArgumentError, /case_contacts: or case_contact_ids: is required/)
+    end
+
+    it "throws an error when both users and user_ids are used" do
+      # expect {
+      #   subject.seed_additional_expenses(case_contacts: CaseContact.all, case_contact_ids: [1, 2])
+      # }.to raise_error(ArgumentError, /cannot use case_contacts: and case_contact_ids:/)
+    end
+
+    it "throws an error when users is not an ActiveRecord::Relation" do
+      # expect {
+      #   subject.seed_additional_expenses(case_contacts: 2)
+      # }.to raise_error(TypeError, /param case_contacts: must be an ActiveRecord::Relation/)
+    end
+
+    it "throws an error when users is an empty ActiveRecord::Relation" do
+      # expect {
+      #   subject.seed_additional_expenses(case_contacts: CaseContact.where(id: -1))
+      # }.to raise_error(ArgumentError, /param case_contacts: must contain at least one case_contact/)
+    end
+
+    it "throws an error when user_ids is not an array" do
+      # expect {
+      #   subject.seed_additional_expenses(case_contact_ids: 2)
+      # }.to raise_error(TypeError, /param case_contact_ids: must be an array/)
+    end
+
+    it "throws an error when user_ids is an empty array" do
+      # expect {
+      #   subject.seed_additional_expenses(case_contact_ids: [])
+      # }.to raise_error(RangeError, /param case_contact_ids: must contain at least one element/)
+    end
+  end
 end
